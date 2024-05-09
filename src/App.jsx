2e0484ebec3ccbc6c1d587a09f64b7e99b46084e
Cliff Pyles <cliff@cliffpyles.com>
2024-05-09 10:26:59 -0400

    Add Data section

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { Grommet, Tab, Tabs } from "grommet";
 import FormsSection from "./sections/Forms";
 import TypographySection from "./sections/Typography";
 import LayoutsSection from "./sections/Layouts";
+import DataSection from "./sections/Data";
 import * as themes from "./themes";
 
 function App() {
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(1);
   return (
     <Grommet theme={themes.standard} full>
       <Tabs activeIndex={activeTab} onActive={setActiveTab}>
         <Tab title="Layouts">
           <LayoutsSection />
+        </Tab>
+        <Tab title="Data">
+          <DataSection />
         </Tab>
         <Tab title="Forms">
           <FormsSection />
